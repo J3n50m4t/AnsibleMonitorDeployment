874d09ec7c2174a5526d9d0464abcda11291064a
Markus Dittrich <markusdittrich1@gmail.com>
2019-11-18 22:57:51 +0100

    improved docker install detection

--- menu.sh
+++ menu.sh
@@ -19,41 +19,35 @@ else
   sudo echo "export NCURSES_NO_UTF8_ACS=1" >> /etc/bash.bashrc.local
 fi
 #check if docker is installed
-docker="/usr/bin/docker"
-if [ -e "$docker"]
+which docker
+if ! [ $? -eq 0 ]
 then
-  clear 1>/dev/null 2>&1
+  docker --version | grep "Docker version"
+  if ! [ $? -eq 0 ]
+  then
+    sudo apt-get update 1>/dev/null 2>&1
+    sudo apt-get remove docker docker-engine docker.io containerd runc 1>/dev/null 2>&1
+    sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y 1>/dev/null 2>&1
+    curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - 1>/dev/null 2>&1
+    sudo apt-key fingerprint 0EBFCD88 1>/dev/null 2>&1
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" 1>/dev/null 2>&1
+    sudo apt-get update 1>/dev/null 2>&1
+    sudo apt-get install docker-ce docker-ce-cli containerd.io -y 1>/dev/null 2>&1
+    sudo apt-get install ansible -y 1>/dev/null 2>&1
+    sudo apt-get install unzip -y 1>/dev/null 2>&1
+    sudo apt-get install curl -y 1>/dev/null 2>&1
+    sudo apt-get install wget -y 1>/dev/null 2>&1
+    sudo apt-get install python-pip -y 1>/dev/null 2>&1
+    sudo pip install docker-py 1>/dev/null 2>&1
+    sudo groupadd docker
+    sudo usermod -aG docker $USER
+    ansible-playbook ./ansiblescripts/deployment.yml --tags network &>/dev/null &
+    ansible-playbook ./ansiblescripts/deployment.yml --tags dockerrestart &>/dev/null &
+  then 
+    clear 1>/dev/null 2>&1
+  fi
 else
-  echo "0" | dialog --gauge "Updating repo" 10 70 0
-  sudo apt-get update 1>/dev/null 2>&1
-  echo "10" | dialog --gauge "Installing docker" 10 70 0
-  sudo apt-get remove docker docker-engine docker.io containerd runc 1>/dev/null 2>&1
-  sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y 1>/dev/null 2>&1
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - 1>/dev/null 2>&1
-  sudo apt-key fingerprint 0EBFCD88 1>/dev/null 2>&1
-  sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" 1>/dev/null 2>&1
-  sudo apt-get update 1>/dev/null 2>&1
-  sudo apt-get install docker-ce docker-ce-cli containerd.io -y 1>/dev/null 2>&1
-  echo "20" | dialog --gauge "Installing ansible" 10 70 0
-  sudo apt-get install ansible -y 1>/dev/null 2>&1
-  echo "30" | dialog --gauge "Installing Unzip" 10 70 0
-  sudo apt-get install unzip -y 1>/dev/null 2>&1
-  echo "40" | dialog --gauge "Installing curl" 10 70 0
-  sudo apt-get install curl -y 1>/dev/null 2>&1
-  echo "50" | dialog --gauge "Installing wget" 10 70 0
-  sudo apt-get install wget -y 1>/dev/null 2>&1
-  echo "60" | dialog --gauge "Installing unionfs-fuse" 10 70 0
-  sudo apt-get install unionfs-fuse -y 1>/dev/null 2>&1
-  echo "70" | dialog --gauge "Installing python-pip" 10 70 0
-  sudo apt-get install python-pip -y 1>/dev/null 2>&1
-  echo "80" | dialog --gauge "Installing docker-py" 10 70 0
-  sudo pip install docker-py 1>/dev/null 2>&1
-  echo "90" | dialog --gauge "Setting up docker and installing" 10 70 0
-  sudo groupadd docker
-  sudo usermod -aG docker $USER
-  ansible-playbook ./ansiblescripts/deployment.yml --tags network &>/dev/null &
-  ansible-playbook ./ansiblescripts/deployment.yml --tags dockerrestart &>/dev/null &
-  echo "100" | dialog --gauge "Everything installed successfuly, going back to main menu" 10 70 0
+  clear 1>/dev/null 2>&1
 fi
 
 OPTIONS=( A "Install Netdata"
