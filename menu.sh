fce12ebfbb2bed3ee81473aebb84117527622363
Markus Dittrich <markusdittrich1@gmail.com>
2019-11-18 22:48:21 +0100

    -y added to install of docker

--- menu.sh
+++ menu.sh
@@ -27,12 +27,12 @@ else
   sudo apt-get update 1>/dev/null 2>&1
   echo "10" | dialog --gauge "Installing docker" 10 70 0
   sudo apt-get remove docker docker-engine docker.io containerd runc 1>/dev/null 2>&1
-  sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common 1>/dev/null 2>&1
+  sudo apt-get install apt-transport-https ca-certificates curl gnupg2 software-properties-common -y 1>/dev/null 2>&1
   curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add - 1>/dev/null 2>&1
   sudo apt-key fingerprint 0EBFCD88 1>/dev/null 2>&1
   sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/debian $(lsb_release -cs) stable" 1>/dev/null 2>&1
   sudo apt-get update 1>/dev/null 2>&1
-  sudo apt-get install docker-ce docker-ce-cli containerd.io 1>/dev/null 2>&1
+  sudo apt-get install docker-ce docker-ce-cli containerd.io -y 1>/dev/null 2>&1
   echo "20" | dialog --gauge "Installing ansible" 10 70 0
   sudo apt-get install ansible -y 1>/dev/null 2>&1
   echo "30" | dialog --gauge "Installing Unzip" 10 70 0
